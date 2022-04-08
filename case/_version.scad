7f329ea1a0a128252d9931dd6c86e0fc0652fd8c
Maximilian Huber <gh@maxhbr.de>
2022-04-08 13:32:49 +0200

    rebuild
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- case/_version.scad
+++ case/_version.scad
@@ -1,2 +1,2 @@
-git_describe="case-v1@0.6";
+git_describe="case-v1@0.6-1-g6605";
 git_commit_count="#C=12";
--- case/build.sh
+++ case/build.sh
@@ -6,26 +6,29 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 writeVersionScad() {
+    local scad="$1"
     cat <<EOF > _version.scad
 git_describe="$(git describe --tags --abbrev=2)";
-git_commit_count="#C=$(git log --oneline "v1.scad" | wc -l)";
+git_commit_count="#C=$(git log --oneline "$scad" | wc -l)";
 EOF
     git add _version.scad
 }
-writeVersionScad
 
 build() (
-    if [[ "$1" && "$1" != "#"* ]]; then
-        local stl="$(echo "$1" | cut -d':' -f1)"
-        local variant="$(echo "$1" | cut -d':' -f2)"
-        local side="$(echo "$1" | cut -d':' -f3)"
+    if [[ "$2" && "$2" != "#"* ]]; then
+        local scad="$1"
+        local stl="$(echo "$2" | cut -d':' -f1)"
+        local variant="$(echo "$2" | cut -d':' -f2)"
+        local side="$(echo "$2" | cut -d':' -f3)"
         if [[ -z "$side" ]]; then
             side="left"
         fi
-        local var_with_cap="$(echo "$1" | cut -d':' -f4)"
+        local var_with_cap="$(echo "$2" | cut -d':' -f4)"
         if [[ -z "$var_with_cap" ]]; then
             var_with_cap=true
         fi
+
+
         mkdir -p "$(dirname "$stl")"
         mkdir -p "ast/$(dirname "$stl")"
 
@@ -35,19 +38,21 @@ build() (
             -D var_variant="\"$variant\"" \
             -D var_side="\"$side\"" \
             -D var_with_cap="$var_with_cap" \
-            "v1.scad"
+            "$scad"
 
         ast_prev="$ast.prev"
         if cmp --silent "$ast" "$ast_prev"; then
             echo "... already generated $stl"
         else
-            set -x
-            openscad --hardwarnings \
+            ( set -x
+              openscad --hardwarnings \
                 -o "$stl" \
                 -D var_variant="\"$variant\"" \
                 -D var_side="\"$side\"" \
                 -D var_with_cap="$var_with_cap" \
-                "v1.scad"
+                "$scad"
+            )
+            diff "$ast" "$ast_prev" > "$ast.diff"
             cp "$ast" "$ast_prev"
         fi
     fi
@@ -55,7 +60,14 @@ build() (
 
 export -f build
 
-cat <<EOF | parallel --progress build {} 1>&2
+###############################################################################
+##  run  ######################################################################
+###############################################################################
+
+scad="v1.scad"
+
+writeVersionScad "$scad"
+cat <<EOF | parallel --progress build "$scad" {} 1>&2
 out/left.topCase.stl:shrouded:left:true
 out/left.topPlate.stl:plate:left:true
 out/left.bottomPlate.stl:bottomPlate:left

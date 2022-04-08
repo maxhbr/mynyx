66059aaad12a0028b5789681a153ddf23ee56f25
Maximilian Huber <gh@maxhbr.de>
2022-04-08 13:24:19 +0200

    update, and caching
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- case/_version.scad
+++ case/_version.scad
@@ -1,2 +1,2 @@
-git_describe="case-v1@0.5-2-ge128";
+git_describe="case-v1@0.6";
 git_commit_count="#C=12";
--- case/build.sh
+++ case/build.sh
@@ -3,6 +3,8 @@
 
 set -euo pipefail
 
+cd "$(dirname "$0")"
+
 writeVersionScad() {
     cat <<EOF > _version.scad
 git_describe="$(git describe --tags --abbrev=2)";
@@ -24,23 +26,30 @@ build() (
         if [[ -z "$var_with_cap" ]]; then
             var_with_cap=true
         fi
-        set -x
         mkdir -p "$(dirname "$stl")"
         mkdir -p "ast/$(dirname "$stl")"
 
+        local ast="ast/${stl%.*}.ast"
         openscad --hardwarnings \
-            -o "ast/${stl%.*}.ast" \
+            -o "$ast" \
             -D var_variant="\"$variant\"" \
             -D var_side="\"$side\"" \
             -D var_with_cap="$var_with_cap" \
             "v1.scad"
 
-        openscad --hardwarnings \
-            -o "$stl" \
-            -D var_variant="\"$variant\"" \
-            -D var_side="\"$side\"" \
-            -D var_with_cap="$var_with_cap" \
-            "v1.scad"
+        ast_prev="$ast.prev"
+        if cmp --silent "$ast" "$ast_prev"; then
+            echo "... already generated $stl"
+        else
+            set -x
+            openscad --hardwarnings \
+                -o "$stl" \
+                -D var_variant="\"$variant\"" \
+                -D var_side="\"$side\"" \
+                -D var_with_cap="$var_with_cap" \
+                "v1.scad"
+            cp "$ast" "$ast_prev"
+        fi
     fi
 )
 
@@ -49,11 +58,9 @@ export -f build
 cat <<EOF | parallel --progress build {} 1>&2
 out/left.topCase.stl:shrouded:left:true
 out/left.topPlate.stl:plate:left:true
-#out/left.topPlate.no-thumb-cluster.stl:no-thumb-cluster:left
 out/left.bottomPlate.stl:bottomPlate:left
 out/right.topCase.stl:shrouded:right:true
 out/right.topPlate.stl:plate:right:true
-#out/right.topPlate.no-thumb-cluster.stl:no-thumb-cluster:right
 out/right.bottomPlate.stl:bottomPlate:right
 EOF
 
